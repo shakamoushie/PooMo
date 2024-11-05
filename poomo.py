eb10c34a41888a97ad6a563d146a4e7b6d4c9e13
shakamoushie <88389632+shakamoushie@users.noreply.github.com>
2024-11-06 01:27:44 +0530

    Add files via upload

--- poomo.py
+++ poomo.py
@@ -11,6 +11,8 @@ st.set_page_config(page_title = "PooMo", page_icon="./burger.png", layout = "wid
 
 # session variables
 if "icons" not in ss: ss.icons = {}
+if "tmpfls" not in ss: ss.tmpfls = {}
+if "starter_func_run_once" not in ss: ss.starter_func_run_once = False
 
 # general variables
 vDrive = os.path.splitdrive(os.getcwd())[0]
@@ -23,7 +25,7 @@ separater_line = f"<hr style='margin-top: 0; margin-bottom: 0; size: 1px; border
 
 # SCTN: Common Utility Functions
 
-def ReduceGapFromPageTop(): st.markdown(" <style> div[class^='block-container'] { padding-top: 0rem; } </style> ", True)
+def ReduceGapFromPageTop(): st.markdown(" <style> div[class^='stMainBlockContainer'] { padding-top: 3rem; } </style> ", True)
 
 def ReadPictureFile(wch_fl, fldr_location="./"):
   try:
@@ -32,49 +34,53 @@ def ReadPictureFile(wch_fl, fldr_location="./"):
 
   except: return ""
 
-def AutoDownload(file_object, mime_type, file_name): # re take from file copy 26Aug24
-  st.toast(f"📥 Downloading as file: {file_name}...")
-  dlink = f"""<html><head>
-                <script src="http://code.jquery.com/jquery-3.2.1.min.js"> </script>
-                <script> $('<a href="data:{mime_type};base64,{file_object}" download="{file_name}">')[0].click() </script>
-              </head></html> """
-  
-  components.html(dlink, height=0, width=0)
+def StarterFunctionRunOnce():
+  icon_array = ["address", "timings", "contacts", "about"]
+  for vicon in icon_array:
+    iconfl = ReadPictureFile(f"{vicon}.png")
+    ss.icons[vicon] = f"""<img src="data:png;base64,{iconfl}" width='20' height='20'>"""
+
+  ss.tmpfls['Menu_png'] = open("./PooMo_Menu.png", 'rb').read()
+  ss.tmpfls['Menu_pdf'] = open("./PooMo_Menu.pdf", 'rb').read()
+
+  imgfl = './LandingPage.jpg'
+  ss.tmpfls['MainImg'] = f"""<img src="data:jpg;base64,{ReadPictureFile(imgfl)}" width='800' height='500'>"""
+
+def PageHome():
+  sc21, sc22 = st.columns((3,1.5))
+  sc21.html(ss.tmpfls['MainImg'])
+  side_code = f"""
+  <h1>PooMo Cafe</h1><br>
+  {ss.icons['about']} About: ... <br><br>
+  {ss.icons['address']} Address: ... <br><br>
+  {ss.icons['timings']} Timings: ... <br><br>
+  {ss.icons['contacts']} Contact: ...
+  """
+  sc22.html(side_code)
 
 def PageMenu():
   ReduceGapFromPageTop()
 
-  sc1, sc2, sc3 = st.columns((1, 1, 6))
-  menu_btn = sc1.button("Menu", icon=":material/restaurant:")
-  dnld_btn = sc2.button("Download", icon=":material/download:")
+  sc1, sc2, sc3, sc4 = st.columns((1, 1, 1, 6))
+  if sc1.button("Home", icon=":material/home:"): 
+    st.markdown(separater_line, True)
+    PageHome()
+
+  if sc2.button("Menu", icon=":material/restaurant:", help="Show Menu"): 
+    st.markdown(separater_line, True)
+    st.image(ss.tmpfls['Menu_png'])
+  
+  sc3.download_button(label="Download", 
+                      icon=":material/download:",
+                      data=ss.tmpfls['Menu_pdf'], 
+                      file_name="PooMo_Downloaded_Menu.pdf", 
+                      mime="application/pdf",
+                      help="Download Menu")
+  
   st.markdown(separater_line, True)
 
-  if menu_btn: image_content = st.image(open("./PooMo_Menu.png", 'rb').read())
-  else:
-    if len(ss.icons) == 0:
-      icon_array = ["address", "timings", "contacts", "about"]
-      for vicon in icon_array:
-        iconfl = ReadPictureFile(f"{vicon}.png")
-        ss.icons[vicon] = f"""<img src="data:png;base64,{iconfl}" width='20' height='20'>"""
-    
-    imgfl = './LandingPage.jpg'
-    img_code = f"""<img src="data:jpg;base64,{ReadPictureFile(imgfl)}" width='900' height='550'>"""
-    sc21, sc22 = st.columns((4,1.5))
-    sc21.html(img_code)
-    side_code = f"""
-    <h1>PooMa Cafe</h1><br>
-    {ss.icons['about']} About: ... <br><br>
-    {ss.icons['address']} Address: ... <br><br>
-    {ss.icons['timings']} Timings: ... <br><br>
-    {ss.icons['contacts']} Contact: ...
-    """
-    sc22.html(side_code)
+if 'runpage' not in ss: 
+  if ss.starter_func_run_once == False: StarterFunctionRunOnce()
+  ss.runpage = PageMenu 
 
-  if dnld_btn:
-    pdf_content = open("./PooMo.pdf", 'rb').read()
-    file_object = base64.b64encode(pdf_content).decode()
-    # AutoDownload(file_object, "application/octet-stream", "PooMo_Menu.pdf")
-    AutoDownload(file_object, "application/pdf", "PooMo_Menu.pdf")
-
-if 'runpage' not in ss: ss.runpage = PageMenu 
 ss.runpage()
