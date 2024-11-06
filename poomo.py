a59503ba343c913e3bcbc24f68db55a6514ea899
shakamoushie <88389632+shakamoushie@users.noreply.github.com>
2024-11-07 00:10:34 +0530

    Add files via upload

--- poomo.py
+++ poomo.py
@@ -1,7 +1,7 @@
 # external library imports
 import streamlit as st
 from streamlit import session_state as ss
-import streamlit.components.v1 as components
+from streamlit_pdf_reader import pdf_reader   # pip install streamlit-pdf-reader
 import base64
 import os
 
@@ -10,7 +10,6 @@ st.set_page_config(page_title = "PooMo", page_icon="./burger.png", layout = "wid
 # Variables  -----------------------------------------------------------------------------------------------------------------------
 
 # session variables
-if "icons" not in ss: ss.icons = {}
 if "tmpfls" not in ss: ss.tmpfls = {}
 if "starter_func_run_once" not in ss: ss.starter_func_run_once = False
 
@@ -35,52 +34,47 @@ def ReadPictureFile(wch_fl, fldr_location="./"):
   except: return ""
 
 def StarterFunctionRunOnce():
-  icon_array = ["address", "timings", "contacts", "about"]
-  for vicon in icon_array:
-    iconfl = ReadPictureFile(f"{vicon}.png")
-    ss.icons[vicon] = f"""<img src="data:png;base64,{iconfl}" width='20' height='20'>"""
-
-  ss.tmpfls['Menu_png'] = open("./PooMo_Menu.png", 'rb').read()
   ss.tmpfls['Menu_pdf'] = open("./PooMo_Menu.pdf", 'rb').read()
 
   imgfl = './LandingPage.jpg'
-  ss.tmpfls['MainImg'] = f"""<img src="data:jpg;base64,{ReadPictureFile(imgfl)}" width='800' height='500'>"""
+  ss.tmpfls['MainImg'] = f"""<img src="data:jpg;base64,{ReadPictureFile(imgfl)}" width='600' height='480'>"""
+
+  imgfl = './cafe title.png'
+  ss.tmpfls['CafeName'] = f"""<img src="data:jpg;base64,{ReadPictureFile(imgfl)}" width='550' height='100'>"""
+
+@st.dialog("View Menu")
+def ShowMenu(): pdf_reader('./PooMo_Menu.pdf')
 
 def PageHome():
-  sc21, sc22 = st.columns((3,1.5))
-  sc21.html(ss.tmpfls['MainImg'])
-  side_code = f"""
-  <h1>PooMo Cafe</h1><br>
-  {ss.icons['about']} About: ... <br><br>
-  {ss.icons['address']} Address: ... <br><br>
-  {ss.icons['timings']} Timings: ... <br><br>
-  {ss.icons['contacts']} Contact: ...
-  """
-  sc22.html(side_code)
-
-def PageMenu():
   ReduceGapFromPageTop()
 
-  sc1, sc2, sc3, sc4 = st.columns((1, 1, 1, 6))
-  if sc1.button("Home", icon=":material/home:"): 
-    st.markdown(separater_line, True)
-    PageHome()
+  st.html(ss.tmpfls['CafeName'])
+  st.markdown(separater_line, True)
 
-  if sc2.button("Menu", icon=":material/restaurant:", help="Show Menu"): 
-    st.markdown(separater_line, True)
-    st.image(ss.tmpfls['Menu_png'])
-  
-  sc3.download_button(label="Download", 
+  sc21, sc22 = st.columns(2)
+  sc22.html(ss.tmpfls['MainImg'])
+
+  with sc21:
+    st.text("⚠️ About: ...")
+    st.text("📌 Address: ...")
+    st.text("🕒 Timings: ...")
+    st.text("☎️ Contact: ...")
+    st.text("🍽️ Menu:")
+
+    sc1, sc2, sc3 = st.columns((1, 1, 2.5))
+    if sc1.button("View", icon=":material/visibility:", help="View Menu"): ShowMenu()
+    sc2.download_button(label="Download", 
                       icon=":material/download:",
                       data=ss.tmpfls['Menu_pdf'], 
                       file_name="PooMo_Downloaded_Menu.pdf", 
                       mime="application/pdf",
                       help="Download Menu")
-  
+
   st.markdown(separater_line, True)
+
 
 if 'runpage' not in ss: 
   if ss.starter_func_run_once == False: StarterFunctionRunOnce()
-  ss.runpage = PageMenu 
+  ss.runpage = PageHome 
 
 ss.runpage()
